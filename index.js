08a5657eec75ca01f5fefbeaff6b7ac08031a8ff
MalikShoh <malikovvv07@gmail.com>
2024-03-04 18:26:47 +0500

    commit2

---
+++ index.js
@@ -0,0 +1,60 @@
+//1
+// for(let index = 1; index < 101; index++){
+//     console.log(index);
+// }
+//2
+// for(let index = -100;index < 0; index++){
+//     console.log(index)
+// }
+//3
+// for(let index = 100;index > 0;index--){
+//     console.log(index);
+// }
+//4
+// for(let index = 1;index <= 100;index++){
+//     if(index % 2 == 0){
+//         console.log(index)
+//     }
+// }
+//5
+// for(let index = 1; index <= 100; index++){
+//     if(index % 3 == 0){
+//         console.log(index)
+//     }
+// }
+//6
+// let arr = []
+// for(let index = 1;index <= 10; index++){
+//     arr.push(index);
+// }
+//7
+// let arr = []
+// for(let index = 1;index <= 100; index++){
+//     if(index % 2 == 0){
+//         arr.push(index);
+//     }
+// }
+// console.log(arr)
+//8
+// const arr = [1,3,-1,-5,2,-2,4,-3];
+// let res = 0;
+// for(let index = 0; index < arr.length; index++){
+//     if(arr[index] < 0){
+//         res++
+//     }
+// }
+// console.log(res)
+//9
+// const arr = [1,3,-1,-5,2,-2,4,-3,2];
+// let res = 0;
+// for(let index = 0;index < arr.length;index++){
+//     if(arr[index] > 0){
+//         res++
+//     }
+// }
+// console.log(res);
+// let str = "hello";
+// if (str.length >= 2) {
+//     str = str.slice(0, -2) + str[4];
+// }
+// console.log(str);
